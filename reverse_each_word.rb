bf49b33a37bd7debf6709ac5d60ccdf48ecde7f6
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-29 21:38:38 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,31 @@
+reverse each word lab: def reverse_each_word(string)
+       # turn string into array by using .split
+    og_array = string.split(" ")
+    return_array = []
+og_array.each do |string|
+   return_array << string.reverse
+        # .reverse is used to reverse an array
+end
+end
+  def reverse_each_word(string)
+    array = string.split(" ") #turn string into an array
+    test_array = []
+    array.collect do|string| #iterate over the array
+      test_array << string.reverse #reverse each word in the array
+    end
+    test_array.join(" ")
+  end
+2:36
+yield and blocks code: def hello_t(array)
+    if block_given?
+i = 0
+while i < array.length
+    yield array[i]
+    i = i + 1
+end
+array
+else
+    puts "Hey! No block was given!"
+end
+end
+# call your method here!
